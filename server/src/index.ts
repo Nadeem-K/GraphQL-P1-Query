9f5bdb1d07959810e7793763edb55dafdc5f6b41
Nadeem Khodabacchus <mr.nadeemk@gmail.com>
2024-04-14 17:15:12 +1200

    Resolver setup for live data

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,30 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./schema";
-import { addMocksToSchema } from "@graphql-tools/mock";
-import { makeExecutableSchema } from "@graphql-tools/schema";
-import { mocks } from "./mock";
+import { resolvers } from "./resolver";
+import { TrackAPI } from "./datasources/track-api";
 
 async function startApolloServer(){
+    // const server = new ApolloServer({
+    //     schema: addMocksToSchema({
+    //         schema: makeExecutableSchema({ typeDefs }),
+    //         mocks
+    //       }),
+    // });
     const server = new ApolloServer({
-        schema: addMocksToSchema({
-            schema: makeExecutableSchema({ typeDefs }),
-            mocks
-          }),
+      typeDefs,
+      resolvers
     });
-    const { url } = await startStandaloneServer(server);
+    const { url } = await startStandaloneServer(server, {
+      context: async () => {
+        const { cache } = server;
+        return {
+          dataSources: {
+            trackAPI: new TrackAPI({ cache }),
+          },
+        };
+      },
+    });
     console.log(`
     🚀  Server is running!
     📭  Query at ${url}
