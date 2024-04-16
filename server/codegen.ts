df90831d65c1ca499b79c13bbb179ef3c3306f66
Nadeem Khodabacchus <mr.nadeemk@gmail.com>
2024-04-16 17:32:29 +1200

    Code generation - authorId

--- server/codegen.ts
+++ server/codegen.ts
@@ -4,7 +4,14 @@ const config: CodegenConfig = {
     schema: "./src/schema.ts",
     generates: {
         "./src/types.ts": {
-            plugins: ["typescript", "typescript-resolvers"],
+          plugins: ["typescript", "typescript-resolvers"],
+          config: {
+            contextType: "./context#DataSourceContext",
+            mappers: {
+                Track: "./models#TrackModel",
+              },
+          },
+          
         },
       },
 };
